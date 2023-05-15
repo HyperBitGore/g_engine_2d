243d635ee08ac2849e42ddee644ae31923bb3db8
HyperBitGore <bruiserjoe01@gmail.com>
2023-05-14 23:48:24 -0500

    0.8.6
    Loca and header loading added. Just need to read actual glyf table now

--- FontRenderer.cpp
+++ FontRenderer.cpp
@@ -148,9 +148,9 @@ int get_glyph_index_format4(UINT16 c, format4* f, UINT16* idRangeStart) {
 }
 
 //c should be at the start of the cmap table so the table.offset works
-void readFormat4(char* c, cmap_table table) {
+void readFormat4(char* c, cmap_table* table) {
 	//now we get to reading, probably make cmap_table have array of glyph indexs so easier to store
-	char* m = c + table.offset;
+	char* m = c + table->offset;
 	UINT16* t = (UINT16*)m;
 	format4 form;
 	form.format = SwapTwoBytes(*t);
@@ -199,13 +199,12 @@ void readFormat4(char* c, cmap_table table) {
 	//now we read all of the character codes
 	UINT16 start = 32;
 	for (start; start <= 563; start++) {
-		table.indexs.push_back({ get_glyph_index_format4(start, &form, idRangeStart), start});
+		table->indexs.push_back({ get_glyph_index_format4(start, &form, idRangeStart), start});
 	}
-	std::cout << table.indexs[0].c << " : " << table.indexs[0].index << "\n";
+	//std::cout << table->indexs[0].c << " : " << table->indexs[0].index << "\n";
 }
 
 //https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6cmap.html
-//need to write support for the formats and figure out how to get format the tables are in
 cmap readCmap(char* c, int offset, int length) {
 	cmap map;
 	char* m = c + offset;
@@ -230,7 +229,7 @@ cmap readCmap(char* c, int offset, int length) {
 		switch (format) {
 		case 4:
 			//most common
-			readFormat4(m, map.tables[i]);
+			readFormat4(m, &map.tables[i]);
 			break;
 		case 0:
 			break;
@@ -258,24 +257,150 @@ cmap readCmap(char* c, int offset, int length) {
 	return map;
 }
 
-void readLoca(char* c, int offset, int length) {
+//stores character code and offset
+struct loca {
+	char c;
+	UINT32 offset;
+};
+
+
+
+//read the table when it is the 16 bit version
+UINT32 readLoca16(char* s, UINT16 index) {
+	UINT16* d = ((UINT16*)s + index);
+	return SwapTwoBytes(*d) * 2;
+}
+//reading the table when it is the 32 bit version
+UINT32 readLoca32(char* s, UINT16 index) {
+	UINT32* d = (UINT32*)s + index;
+	return SwapFourBytes(*d);
+}
+
+std::vector<loca> readLoca(char* c, int offset, int length, UINT16 format, cmap* map) {
+	char* m = c + offset;
+	std::vector<loca> locas;
+	if (format == 0) {
+		for (int i = 0; i < map->tables[0].indexs.size(); i++) {
+			loca l;
+			l.c = map->tables[0].indexs[i].c;
+			l.offset = readLoca16(m, map->tables[0].indexs[i].index);
+			locas.push_back(l);
+		}
+	}
+	else {
+		for (int i = 0; i < map->tables[0].indexs.size(); i++) {
+			loca l;
+			l.c = map->tables[0].indexs[i].c;
+			l.offset = readLoca32(m, map->tables[0].indexs[i].index);
+			locas.push_back(l);
+		}
+	}
+	return locas;
+}
+
+//most of data in here doesn't matter for my uses
+struct TTFHeader {
+	UINT32 version; //supposed to be fixed but idrc
+	UINT32 fontRevision; //set by manufacturer
+
+	UINT32 checkSumAdjustment; //checksum for file; Have to sum entire file as an uint32 and then do 0xB1B0AFBA - sum; 
+	UINT32 magicNumber; //idek what this is for supposed to be 0x5F0F3CF5
+
+	UINT16 flags; //every bit gives u flag; These flags can be complex as shit
+	UINT16 uintsPerEm; //how may FUnits are in 1 em
+
+	time_t  created; //self explanatory
+	time_t modified; //also self explanatory
+
+	short xMin; //supposed to be an FWord but that is just 16 bit signed integer so a short; Also in FUnits
+	short yMin;
+	short xMax;
+	short yMax;
+
+	UINT16 macStyle; //each bit has parameters
+	UINT16 lowestRecPPEM; //smallest readable size in pixels
+	short fontDirectionHint; //give you hint for directions of glyphs
+	short indexToLocFormat; //tells you format of loca table
+	short glyphDataFormat; //0 is current format; All i know
+};
+
+
+
+TTFHeader readHead(char* c, int offset, int length) {
+	TTFHeader head;
+	char* m = c + offset;
+	UINT32* u = (UINT32*)m;
+	head.version = SwapFourBytes(*u); //can't swap the bytes on a double for some reason
+	u++;
+	head.fontRevision = SwapFourBytes(*u); //values will be wrong for the top two since supposed to be fixed
+	u++;
+	head.checkSumAdjustment = SwapFourBytes(*u);
+	u++;
+	head.magicNumber = SwapFourBytes(*u);
+	u++;
+	UINT16* t = (UINT16*)u;
+	head.flags = *t; //don't swap this since the bits need to be the same
+	t++;
+	head.uintsPerEm = SwapTwoBytes(*t);
+	t++;
+	time_t* p = (time_t*)t;
+	head.created = SwapEightBytes(*p);
+	p++;
+	head.modified = SwapEightBytes(*p);
+	p++;
+	short* s = (short*)p;
+	head.xMin = SwapTwoBytes(*s);
+	s++;
+	head.yMin = SwapTwoBytes(*s);
+	s++;
+	head.xMax = SwapTwoBytes(*s);
+	s++;
+	head.yMax = SwapTwoBytes(*s);
+	s++;
+	t = (UINT16*)s;
+	head.macStyle = *t; //don't swap cause need same bits for flags
+	t++;
+	head.lowestRecPPEM = SwapTwoBytes(*t);
+	t++;
+	s = (short*)t;
+	head.fontDirectionHint = SwapTwoBytes(*s); 
+	s++;
+	head.indexToLocFormat = SwapTwoBytes(*s);
+	s++;
+	head.glyphDataFormat = SwapTwoBytes(*s);
+	s++;
+	return head;
+}
+
+
+
+//glyf time
+
+void readGlyf(char* c, int offset, int length) {
 
 }
 
 void readDirectorys(font_dir* directory, Font* f, char* c) {
+	//need to sort directorys so you do them in right order
+	cmap c_map;
+	TTFHeader header;
+	std::vector<loca> locas;
 	for (auto& i : directory->table) {
 		if (i.t.compare("cmap") == 0) {
-			readCmap(c, i.offset, i.length);
+			c_map = readCmap(c, i.offset, i.length);
+		}
+		else if (i.t.compare("head") == 0){
+			header = readHead(c, i.offset, i.length);
 		}
 		else if (i.t.compare("loca") == 0) {
-			readLoca(c, i.offset, i.length);
+			locas = readLoca(c, i.offset, i.length, header.indexToLocFormat, &c_map);
 		}
 	}
 }
 
 
 //https://docs.fileformat.com/font/ttf/
-//https://handmade.network/forums/articles/t/7330-implementing_a_font_reader_and_rasterizer_from_scratch%252C_part_1__ttf_font_reader. part 8
+//https://handmade.network/forums/articles/t/7330-implementing_a_font_reader_and_rasterizer_from_scratch%252C_part_1__ttf_font_reader. part 12
 //https://tchayen.github.io/posts/ttf-file-parsing
 // https://github.com/RazrFalcon/ttf-parser
 //https://learn.microsoft.com/en-us/typography/opentype/spec/ttch01
